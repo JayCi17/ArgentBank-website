224479c3daad876a364aefc646c3904b4a83cf29
jay-ci <jeancharlesvalentin13@gmail.com>
2023-10-09 15:22:46 +0200

    création du footer

--- argent_bank/src/App.js
+++ argent_bank/src/App.js
@@ -1,13 +1,12 @@
 import Header from "./layouts/header";
+import Footer from "./layouts/footer";
 
 function App() {
   return (
-    <>
     <div className="container">
       <Header/>
+      <Footer/>
     </div>
-    </>
   );
 }
-
 export default App;
