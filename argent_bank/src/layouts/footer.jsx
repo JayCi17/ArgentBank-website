d336245a084e9ffe9c27f5f5a58491931b758bbd
jay-ci <jeancharlesvalentin13@gmail.com>
2023-10-09 15:24:01 +0200

    ajout du footer

---
+++ argent_bank/src/layouts/footer.jsx
@@ -0,0 +1,13 @@
+
+
+
+
+function Footer(){
+    return(
+        <footer className="footerContainer">
+            <p className="copyright">Copyright 2020 Argent Bank</p>
+        </footer>
+    )
+}
+
+export default Footer;
