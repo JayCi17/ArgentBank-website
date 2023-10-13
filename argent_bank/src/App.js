2e3dec41ef9f6e5277e3ea4b9364028e1c9b0192
jay-ci <jeancharlesvalentin13@gmail.com>
2023-10-13 12:24:54 +0200

    création de la route vers signin

--- argent_bank/src/App.js
+++ argent_bank/src/App.js
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/home";
+import SignIn from "./pages/signin";
 import Header from "./layouts/header";
 import Footer from "./layouts/footer";
 import "./styles/main.scss"
@@ -10,6 +11,7 @@ function App() {
       <Header/>
       <Routes>
         <Route path="/" element={<Home/>}/>
+        <Route path="/signin" element={<SignIn/>}/>
       </Routes>
       <Footer/>
     </div>
