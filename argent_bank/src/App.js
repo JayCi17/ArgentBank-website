69d6d2ac5433ea73642303f7520a6dd9723b0738
jay-ci <jeancharlesvalentin13@gmail.com>
2023-10-23 10:47:15 +0200

    modification de la taille des images

--- argent_bank/src/App.js
+++ argent_bank/src/App.js
@@ -15,7 +15,7 @@ function App() {
         <Route path="/" element={<Home/>}/>
         <Route path="/signin" element={<SignIn/>}/>
         <Route path="/signup" element={<SignUp/>}/>
-        <Route path="/user" element={<User/>}/>
+        <Route path="/user" element={<User/>}/>  
       </Routes>
       <Footer/>
     </div>
